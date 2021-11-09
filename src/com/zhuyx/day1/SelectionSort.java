d78c09719bfc28c70343e0715a4d6995dc5ab2b7
baixiaoseng <liyani6u@163.com>
2021-11-09 13:18:31 +0800

    需求号：1 更改内容：新增选择排序 更改原因：学习练习 更改人：c_zhuyx02

---
+++ src/com/zhuyx/day1/SelectionSort.java
@@ -0,0 +1,23 @@
+package com.zhuyx.day1;
+
+public class SelectionSort {
+
+    public void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
