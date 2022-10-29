aa344d332508c64d286b349963156ff8ad2411b4
baixiaoseng <liyani6u@163.com>
2022-10-29 09:02:06 +0800

    需求号：1 更改内容：新增选择排序 更改原因：学习练习 更改人：c_zhuyx02

--- src/com/zhuyx/day1/MergeSort.java
+++ src/com/zhuyx/day1/MergeSort.java
@@ -1,2 +1,23 @@
-package com.zhuyx.day1;public class MergeSort {
+package com.zhuyx.day1;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+    }
+    private void process(int[] arr,int L,int R){
+        if(L == R){
+            return;
+        }
+        int mid = L + (R-L)>>1+1;
+        process(arr,L,mid);
+        process(arr,mid+1,R);
+        merge(arr,L,mid,R);
+
+    }
+
+    private void merge(int[] arr, int L, int mid, int r) {
+        int[] help = new int[10];
+        
+    }
 }
